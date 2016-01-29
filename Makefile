4e766ed3fd44faf04c9c3ed0583301df7fdbba9f
Irfan Adilovic <i.adilovic@nfotex.com>
2016-01-29 22:47:49 +0100

    Rename safe_cast_test.cc to safe_cast.cc

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 CXXFLAGS += -Wall -Wextra -pedantic -Wno-unused-parameter
 
-all: safe_cast_test
+all: safe_cast
--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ for stdflag in '' -ansi -std=c++11 -std=gnu++11; do
     CXXFLAGS=$stdflag make -B
     echo
     echo "Testing standards flag '$stdflag'"
-    ./safe_cast_test
+    ./safe_cast
 done
